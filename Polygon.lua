93bf3ef8f13586d52201e93432736496c6cbbca4
lusiasxqz <126590236+lusiasxqz@users.noreply.github.com>
2024-09-07 12:27:54 +0700

    Update Polygon.lua

--- Polygon.lua
+++ Polygon.lua
@@ -636,13 +636,22 @@ AncientOneQuest:OnChanged(function()
 end)
 
 spawn(function()
-    while wait() do
-        pcall(function()
+    pcall(function()
+        while wait() do
             if _G.AncientOne_Quest then
                 game:GetService("VirtualInputManager"):SendKeyEvent(true,"Y",false,game)
 				wait(0.1)
                 game:GetService("VirtualInputManager"):SendKeyEvent(false,"Y",false,game)
                 game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("UpgradeRace","Buy")
+            end
+        end
+    end)
+end)
+
+spawn(function()
+    while wait() do
+        pcall(function()
+            if _G.AncientOne_Quest then
                 if game:GetService("Workspace").Enemies:FindFirstChild("Reborn Skeleton") or game:GetService("Workspace").Enemies:FindFirstChild("Living Zombie") or game:GetService("Workspace").Enemies:FindFirstChild("Domenic Soul") or game:GetService("Workspace").Enemies:FindFirstChild("Posessed Mummy") then
                     for i,v in pairs(game:GetService("Workspace").Enemies:GetChildren()) do
                         if v.Name == "Reborn Skeleton" or v.Name == "Living Zombie" or v.Name == "Demonic Soul" or v.Name == "Posessed Mummy" then
